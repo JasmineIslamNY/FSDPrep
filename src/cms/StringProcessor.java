de7d2958495d70935be2910bbf255eda0bef62cc
JasmineIslamNY <jasmine@tek-genie.com>
2016-08-29 13:49:17 -0400

    20160829_1349 (CMS - Completed String processor class)

--- src/cms/StringProcessor.java
+++ src/cms/StringProcessor.java
@@ -1,18 +1,75 @@
 package cms;
+import  java.util.StringTokenizer
 
 public class StringProcessor {
 	
 	public String[] process (String order) {
+		String [] result;
+
 		/**
 		 * This class processes the string command entered by a user/system.  
 		 * It checks the command for size (255 or less) 
 		 * Breaks it into component pieces
-		 * Confirms dealer and command are valid (number of pieces make sense for the command)
 		 * Uses try/catch to convert price and amount 
 		 * Returns String array to caller
 		 */
 		
+		//check length of command user input is 255 or less
+		if (order.length() > 255) {
+			result = new String[]{"Error", "Command length is more than 255 characters"};
+			return result;
+		}
 		
+		// take the string that user input and break it up into an array 
+		result = tokenize(order);
+		
+		//check that Price can be converted to Double
+		if (result[1] == "POST") {
+			try {
+				Double d = Double.parseDouble(result[5]);
+			}
+			catch (NumberFormatException e){
+				result[0] = "ERROR";
+				result[1] = "Price was not input correctly";
+				
+			}
+		}
+		
+		//check that Amount can be converted to Integer for POST
+		if (result[1] == "POST") {
+			try {
+				Integer i = Integer.parseInt(result[4]);
+			}
+			catch (NumberFormatException e){
+				result[0] = "ERROR";
+				result[1] = "Amount was not input correctly";
+				
+			}
+		}
+		//check that Amount can be converted to Integer for AGGRESS
+		if (result[1] == "AGGRESS") {
+			try {
+				Integer i = Integer.parseInt(result[3]);
+			}
+			catch (NumberFormatException e){
+				result[0] = "ERROR";
+				result[1] = "Amount was not input correctly";
+				
+			}
+		}
+		
+		return result;
+		
+	}
+	
+	private String[] tokenize(String order) {
+		StringTokenizer tok = new StringTokenizer(order);
+		int numberOfTokens = tok.countTokens();
+		String [] tempResult = new String[numberOfTokens];
+		for (int i=0; i < numberOfTokens; i++) {
+			tempResult[i] = tok.nextToken();
+		}
+		return tempResult;
 	}
 
 
