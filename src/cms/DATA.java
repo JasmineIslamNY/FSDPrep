5eb96405ac922bab60ca90150f50ac130725ffaf
JasmineIslamNY <jasmine@tek-genie.com>
2016-08-29 21:34:50 -0400

    20160829_2134 (CMS - DATA.add)

--- src/cms/DATA.java
+++ src/cms/DATA.java
@@ -7,8 +7,7 @@ public class DATA {
 	static Integer orderCount = 0;
 	static String [] dealerID = new String[] {"DB", "JPM", "UBS", "RBC", "BARX", "MS", "CITI", "BOFA", "RBS", "HSBC"};
 	static String [] commodity = new String[] {"GOLD", "SILV", "PORK", "OIL", "RICE"};
-	private static HashMap<Integer, Array> dBase = new HashMap(100);
-	@SuppressWarnings("unchecked")
+	private static HashMap<Integer, String[]> dBase = new HashMap(100);
 	private static HashMap<String, ArrayList<Integer>> dBaseByDealerID = new HashMap(100);
 	private static HashMap<String, ArrayList<Integer>> dBaseByCommodity = new HashMap(100);
 	
@@ -22,6 +21,7 @@ public class DATA {
 			result = "UNKNOWN_COMMODITY \n";
 			return result;
 		}
+		
 		//add entry to database of outstanding orders
 		//Key is the orderCount and value is the array of the order
 		dBase.put(orderCount, addOrder);
@@ -32,14 +32,30 @@ public class DATA {
 		 * 2b. If it does not exist, create a new ArrayList, add the new orderID to it, and then add a key/value pair to the HashMap
 		 */
 		if (dBaseByDealerID.containsKey(addOrder[0])) {
-			ArrayList<Integer> arrayList = (ArrayList) dBaseByDealerID.get(addOrder[0]);
+			ArrayList<Integer> arrayList = (ArrayList<Integer>) dBaseByDealerID.get(addOrder[0]);
 			arrayList.add(orderCount);
 			dBaseByDealerID.replace(addOrder[0], arrayList);
 		}
 		else {
-			ArrayList<Integer> arrayList = new ArrayList();
+			ArrayList<Integer> arrayList = new ArrayList<Integer>();
 			arrayList.add(orderCount);
 			dBaseByDealerID.put(addOrder[0], arrayList);
+		}
+		
+		/*Add an index to keep track of orders by commodity.  It's a hashmap where the key is the commodity and the  value is a ArrayList of the keys of dBase 
+		 * 1. First check if it already exists
+		 * 2a. If exists, retrieve ArrayList, add the new orderID to it, and then replace the existing key/value pair
+		 * 2b. If it does not exist, create a new ArrayList, add the new orderID to it, and then add a key/value pair to the HashMap
+		 */
+		if (dBaseByCommodity.containsKey(addOrder[2])) {
+			ArrayList<Integer> arrayList = (ArrayList<Integer>) dBaseByCommodity.get(addOrder[2]);
+			arrayList.add(orderCount);
+			dBaseByCommodity.replace(addOrder[2], arrayList);
+		}
+		else {
+			ArrayList<Integer> arrayList = new ArrayList<Integer>();
+			arrayList.add(orderCount);
+			dBaseByCommodity.put(addOrder[2], arrayList);
 		}
 		
 		
