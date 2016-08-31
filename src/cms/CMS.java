0d1a5f5ea3ed1468b43e42e965d05950d041fad7
JasmineIslamNY <jasmine@tek-genie.com>
2016-08-31 11:30:06 -0400

    20160831_1130 (CMS)

--- src/cms/CMS.java
+++ src/cms/CMS.java
@@ -3,7 +3,9 @@
  * @description This is the starting point for the application.  main method will have three options, base (no extensions), ext1 (run on single TCP connection), ext2 (create multiple TCP connections). 
  */
 package cms;
+
 import java.io.*;
+import java.net.*;
 
 public class CMS {
 
@@ -23,8 +25,9 @@ public class CMS {
 			processKeyboardInput();
 		}
 		else if (args[0].equals("ext1")) {
-			System.out.println("ext1 has not been implemented yet \n");
-			System.exit(1);
+			System.out.println("Selected ext1 \n");
+			processSingleTCPConnection();
+			//System.exit(1);
 		}
 		else if (args[0].equals("ext2")) {
 			System.out.println("ext2 has not been implemented yet \n");
@@ -64,5 +67,40 @@ public class CMS {
 		{
 	      		System.err.println(e);						
 		}	
+	}
+	
+	public static void processSingleTCPConnection() {
+		String clientSentence;
+        String processedSentence;
+        BLOGIC bLogic = new BLOGIC();
+        
+        try {
+			ServerSocket welcomeSocket = new ServerSocket(6789);
+
+			while(true)
+			{
+			   Socket connectionSocket = welcomeSocket.accept();
+			   BufferedReader inFromClient =
+			      new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			   DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			   
+			   outToClient.writeBytes("Thank you for using the Commodity Market System (CMS) \n");
+			   outToClient.writeBytes("Enter command and press enter: (or enter to quit) \n");
+			   
+			   clientSentence = inFromClient.readLine();
+			   System.out.println("Received: " + clientSentence);
+			   if (clientSentence.equals("")) {	
+				   welcomeSocket.close();
+				   System.exit(0);
+			   }
+			  
+			   processedSentence = bLogic.processor(clientSentence);
+			   outToClient.writeBytes(processedSentence);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
---
+++ src/cms/TCPClient.java
@@ -0,0 +1,30 @@
+package cms;
+
+import java.io.*;
+import java.net.*;
+
+class TCPClient {
+	public static void main(String argv[]) throws Exception {
+		String sentence;
+		String messageFromServer;
+		
+		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in) );
+		
+		Socket clientSocket = new Socket("localhost", 6789);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		while (true) {
+			sentence = inFromUser.readLine();
+			
+			if (sentence.equals("")) {						
+      			System.exit(0);
+      			clientSocket.close();
+			}
+			
+			outToServer.writeBytes(sentence + '\n');
+			messageFromServer = inFromServer.readLine();
+			System.out.println(messageFromServer);
+		}
+	}
+}
